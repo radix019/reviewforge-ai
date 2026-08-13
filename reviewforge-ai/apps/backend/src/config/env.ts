6d04380af4d09ef4de0cc97214c7dea4fb39cad1
Rahul Singh <radix018@outlook.com>
2026-08-13 19:12:19 +0530

    refactor: added .env exmaple, added github client id and secret with callback for implementing OAuth

--- reviewforge-ai/apps/backend/src/config/env.ts
+++ reviewforge-ai/apps/backend/src/config/env.ts
@@ -6,4 +6,7 @@ export const env = {
   PORT: process.env.PORT || "5000",
   JWT_SECRET: process.env.JWT_SECRET!,
   DATABASE_URL: process.env.DATABASE_URL!,
+  GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID!,
+  GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET!,
+  GITHUB_CALLBACK_URL: process.env.GITHUB_CALLBACK_URL!,
 };
